89d7517d6f5559de80eca4483922e6ac39e49957
stern <sterniichuk.serhii@gmail.com>
2023-10-20 11:17:06 +0300

    implemented question service

--- expert-system-service/src/main/java/com/example/expertsystemservice/repository/RuleRepository.java
+++ expert-system-service/src/main/java/com/example/expertsystemservice/repository/RuleRepository.java
@@ -1,9 +1,11 @@
 package com.example.expertsystemservice.repository;
 
+import com.example.expertsystemservice.domain.Question;
 import com.example.expertsystemservice.domain.Rule;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -42,6 +44,38 @@ public interface RuleRepository extends Neo4jRepository<Rule, Long> {
     void deleteByName(String name);
 
     Optional<Rule> findRuleById(long id);
+
+    @Query("""
+            MATCH (n:Rule)
+            WHERE NOT ()-[:GOTO]->(n)
+            MATCH (anyRule:Rule)
+            WITH COLLECT(n) AS rootRules, COLLECT(anyRule) AS allRules
+            WHERE NOT ALL(node IN allRules WHERE node IN rootRules)
+            RETURN allRules[0..:#{#limit}]
+            """)
+    List<Question> findAllLimited(long limit);
+    //goal of the query is to provide user with root rules or
+    // if roots too few
+
+    @Query("""
+                MATCH (r:Rule)
+                WHERE toLower(r.name) CONTAINS toLower(:#{#string})
+                OR toLower(r.condition) CONTAINS toLower(:#{#string})
+                RETURN r
+                LIMIT :#{#limit}
+            """)
+    List<Question> findRuleByContainingString(String string, long limit);
+
+
+    @Query("""
+                MATCH (r:Rule)
+                WHERE ANY(searchString IN :#{#strings} WHERE toLower(r.name) CONTAINS searchString
+                OR toLower(r.condition) CONTAINS searchString)
+                RETURN r
+                LIMIT :#{#limit}
+            """)
+    List<Question> findRuleByContainingStringFromArray(String[] strings, long limit);
+
 
     @Query("""
             MATCH (r:Rule)
