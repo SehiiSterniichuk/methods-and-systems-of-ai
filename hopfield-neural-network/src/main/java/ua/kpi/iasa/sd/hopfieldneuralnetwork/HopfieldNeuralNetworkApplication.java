eecd8f1f2e1defbb3168ba434df72dea0321afbd
stern <sterniichuk.serhii@gmail.com>
2023-11-13 16:42:02 +0200

    fix: hopfield network init commit

---
+++ hopfield-neural-network/src/main/java/ua/kpi/iasa/sd/hopfieldneuralnetwork/HopfieldNeuralNetworkApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.iasa.sd.hopfieldneuralnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopfieldNeuralNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HopfieldNeuralNetworkApplication.class, args);
+    }
+
+}
